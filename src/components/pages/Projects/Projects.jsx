9186c4de4a1c5c587482420d5bea65707d204b11
purplebus <mailtosummer@gmail.com>
2023-01-09 10:28:52 +1100

    add second project

--- src/components/pages/Projects/Projects.jsx
+++ src/components/pages/Projects/Projects.jsx
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
 import { Card } from "primereact/card";
 import MetImage from "../../../img/Met.png";
-import { Wrapper, BlogPost, BlogImage } from "./Projects.styles";
+import { BlogPost, BlogImage, GithubRepo } from "./Projects.styles";
+import GithubImage from "../../../img/logos/GitHub-Mark-64px.png";
 
-// links: <a href="http://artworkfromthemet.netlify.app/">;
-// title: "The Metropolitan Museum of Art Collection API",
-// image: <img src={MetImage} alt="Met" />,
 
 export class CardDemo extends Component {
   render() {
@@ -19,22 +17,78 @@ export class CardDemo extends Component {
         }
       ></BlogImage>
     );
+    const header1 = (
+      <BlogImage
+        src={MetImage}
+        alt="Met"
+        onError={(e) =>
+          (e.target.src =
+            "https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png")
+        }
+      ></BlogImage>
+    );
 
     return (
-      <BlogPost>
+      <>      <BlogPost>
+        <a href="http://artworkfromthemet.netlify.app/"        target="_blank"
+        rel="noreferrer">
+          <Card
+            title="The Metropolitan Museum of Art Collection API"
+            style={{
+
+              fontSize: "30px",
+              textAlign: "center",
+            }}
+            header={header}
+          >
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Inventore sed consequuntur error repudiandae numquam deserunt
+              quisquam repellat libero asperiores earum nam nobis, culpa ratione
+              quam perferendis esse, cupiditate neque quas!
+            </p>
+
+          </Card>
+        </a>
+        <GithubRepo>
+        <a href="https://github.com/Wade008/hackathon" target="_blank" rel="noreferrer">
+        {" "}
+
+          <img src={GithubImage} alt="GitHub" />
+
+      </a>
+        </GithubRepo>
+      </BlogPost>
+
+    <BlogPost>
+      <a href="http://artworkfromthemet.netlify.app/"        target="_blank"
+      rel="noreferrer">
         <Card
           title="The Metropolitan Museum of Art Collection API"
-          subTitle="Subtitle"
-          header={header}
+          style={{
+
+            fontSize: "30px",
+            textAlign: "center",
+          }}
+          header={header1}
         >
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
-            sed consequuntur error repudiandae numquam deserunt quisquam
-            repellat libero asperiores earum nam nobis, culpa ratione quam
-            perferendis esse, cupiditate neque quas!
+new text
           </p>
+
         </Card>
-      </BlogPost>
-    );
-  }
-}
+      </a>
+      <GithubRepo>
+      <a href="https://github.com/Wade008/hackathon" target="_blank" rel="noreferrer">
+      {" "}
+
+        <img src={GithubImage} alt="GitHub" />
+
+    </a>
+      </GithubRepo>
+    </BlogPost>
+    </>
+
+  );
+        }
+      }
--- src/components/pages/Projects/Projects.styles.jsx
+++ src/components/pages/Projects/Projects.styles.jsx
@@ -3,94 +3,43 @@ import styled from "styled-components";
 export const Wrapper = styled.div``;
 
 export const BlogImage = styled.img`
-    width: 30vw;
+  width: 20vw;
+  border-radius: 10%;
 
   margin-left: auto;
   margin-right: auto;
-  margin-top: 2vw;
-  border: 2px solid black;
-  border-radius: 10%;
   :hover {
     opacity: 40%;
     transition: 0.3s;
     transform: scale(1.1);
   }
-
-
-
-`
+`;
 
 export const BlogPost = styled.div`
-    width: 30vw;
+  margin-top: 2vw;
+  border-bottom: 1px solid black;
+  width: 30vw;
   margin-left: auto;
   margin-right: auto;
-  
+  margin-bottom: 2em;
 
-
-  p {
-    padding: 0.5vw;
+  a:hover,
+  a:visited,
+  a:link,
+  a:active {
+    text-decoration: none;
     color: black;
   }
 
-`;
+  p {
 
-export const ProjectContent = styled.div`
-  border: 2px dotted $primary1;
-  padding: 1vw;
-  margin: 0.5vw;
-  a:link {
-    text-decoration: none;
-  }
-  h2 {
-    text-shadow: none;
-    text-decoration: none;
-  }
-  h5 {
-    font-size: 0.7em;
-    color: #5b666c;
-    margin: 1vw;
+    text-align: justify;
   }
 `;
 
-export const Project = styled.div``;
+export const GithubRepo = styled.div`
+img{
+  width: 30px;
+}
 
-export const ProjectImage = styled.img`
-  width: 200px;
-  border-radius: 25%;
-  :hover {
-    opacity: 40%;
-    transition: 0.3s;
-    transform: scale(1.1);
-  }
-`;
-
-// // Blog
-// div.blog-post {
-
-//     .picture-post {
-//     display: flex;
-//         img {
-
-//         p {
-
-//         }
-
-//         @media screen and (max-width: 600px) {
-//             width: 40%;
-//             object-fit: contain;
-//         }
-
-//         @media screen and (max-width: 800px) {
-//             width: 30%;
-//             object-fit: contain;
-//         }
-//         }
-//         p {
-//             padding: 0.5vw;
-//             color: black;
-//         }
-//         img:hover {
-//         @include hover_img;
-//         }
-//     }
-// }
+`
